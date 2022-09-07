cdf6fbb9bc8de89bee483ca8a97454cc409e17df
pythonemus <n.dambrosio67@gmail.com>
2022-09-07 17:29:32 +0000

    test

---
+++ .devcontainer/lesson1.py
@@ -0,0 +1,20 @@
+#Lession #1 - Validate working python container
+
+'''
+In this lesson we will be working from a docker container for developement.
+Python code will be executed from a docker container rather than from the local machine.
+
+This container allows for the developer to have greater:
+  - flexibility with application development
+  - more cross platform services
+  - develop a greater sense of microservices.
+
+'''
+
+#Things you can try:
+
+#1.
+
+HELLO_WORLD = 'Hellow World'
+
+print(HELLO_WORLD)
