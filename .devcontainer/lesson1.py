a946f7ae8ccfbbc35bbb796adf854c4f51769eb0
pythonemus <n.dambrosio67@gmail.com>
2022-09-08 02:22:08 +0000

    remove vscode from git

--- .devcontainer/lesson1.py
+++ .devcontainer/lesson1.py
@@ -1,19 +1,23 @@
-#Lession #1 - Validate working python container
+#Lession #1 - Validate working python container/environment
 
 '''
-In this lesson we will be working from a docker container for developement.
-Python code will be executed from a docker container rather than from the local machine.
+In this lesson we will be working from either a development docker container or on your local PC.
+A development docker container can help you execute youe Python rather than from the local machine.
+Running the Docker engine may require more compute resource than what is available
 
-This container allows for the developer to have greater:
-  - flexibility with application development
-  - more cross platform services
-  - develop a greater sense of microservices.
+The alternative option is to install the Python Engine on your local machine
 
 '''
 
 #Things you can try:
+#
+# 1. from your terminal window execute the python script to print the message
+#     python lesson1.py
+#
+# 2. Create a Break Point on the line 22
+#     Run the script from the debugger.
+#     When the script stop at the break point. Use the 'Step into' option to view the variable
 
-#1.
 
 HELLO_WORLD = 'Hellow World'
 
